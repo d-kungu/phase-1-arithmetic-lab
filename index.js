7edc90b479e3c1e6da49ab8d89c35e6fcb2695b3
Andrew Kungu <drewskungu@gmail.com>
2025-03-15 00:51:22 +0300

    MAth!

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const multiply = 62;
+let num1 = 31;
+let num2= 2;
+parseInt(num1);
+parseInt(num2);
+num1 * num2 === multiply
 
+let random = 0;
+random++;
+ 
 // Write your code here
+
+const mod = 4;
+let num3 = 25;
+let num4 = 7;
+num3 % num4 === mod;
+
+const max = 20 
+Math.max(3,4,5,6,20,12,2,5,7,7,8,10) === max
